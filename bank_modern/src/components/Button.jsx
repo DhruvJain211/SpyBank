8a54aeed9d32f18933c36684645c647cce8bf051
DhruvJain211 <dhruvyahoo112@gmail.com1>
2025-08-02 16:03:08 +0530

    made a modern ui

---
+++ bank_modern/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ styles }) => (
+  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+    <a href="https://spend-sync-5dmp.vercel.app/" target="_blank">
+    Get Started
+    </a>
+  </button>
+);
+
+export default Button;
